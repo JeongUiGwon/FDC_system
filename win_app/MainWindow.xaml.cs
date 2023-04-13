c6bcfc2dc407229570ab1de7082c137fd2a6892c
Jeong UiGwon <jek9412@naver.com>
2023-04-13 15:21:49 +0900

    🥑[FE: feat] SignUp UI
    회원가입 UI 구현
    
    Related to: S08P31A201-9

---
+++ win_app/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) 
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
