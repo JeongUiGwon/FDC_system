e936dc4d0e91298562e24baf6a12f0a0a31ebfcf
Jeong UiGwon <jek9412@naver.com>
2023-04-17 17:57:58 +0900

    🥑[FE:feat] DashBoard UI
    대쉬보드 UI 구현

---
+++ win_app/View/Order.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SOM.View
+{
+    /// <summary>
+    /// Order.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Order : UserControl
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+
+        public string TitleOrder
+        {
+            get { return (string)GetValue(TitleOrderProperty); }
+            set { SetValue(TitleOrderProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleOrderProperty = DependencyProperty.Register("TitleOrder", typeof(string), typeof(InfoCard));
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty = DependencyProperty.Register("Desc", typeof(string), typeof(Order));
+
+        public FontAwesome.Sharp.IconChar Icon
+        {
+            get { return (FontAwesome.Sharp.IconChar)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Title", typeof(FontAwesome.Sharp.IconChar), typeof(Order));
+    }
+}
