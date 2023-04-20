7c5446cdac4d8a9323934ff3dfc6d5c10ed5c459
Jeong UiGwon <jek9412@naver.com>
2023-04-20 15:33:39 +0900

    🥑[FE:feat] Data/Interlock UI
    Data/ Interlock UI 구현
    
    Related to: S08P31A201-140, S08P31A201-141

--- win_app/View/MainWindow.xaml.cs
+++ win_app/View/MainWindow.xaml.cs
@@ -79,6 +79,19 @@ namespace SOM
             this.frame.Navigate(new Uri("/View/RecipePage.xaml", UriKind.Relative));
         }
 
+        private void Btn_Data_Click(object sender, RoutedEventArgs e)
+        {
+            this.Btn_Dashboard.Style = Application.Current.Resources["menuButton"] as Style;
+            this.Btn_Equipments.Style = Application.Current.Resources["menuButton"] as Style;
+            this.Btn_Params.Style = Application.Current.Resources["menuButton"] as Style;
+            this.Btn_Recipe.Style = Application.Current.Resources["menuButton"] as Style;
+            this.Btn_Datas.Style = Application.Current.Resources["menuButtonActive"] as Style;
+            this.Btn_Interlock.Style = Application.Current.Resources["menuButton"] as Style;
+            this.Btn_Users.Style = Application.Current.Resources["menuButton"] as Style;
+
+            this.frame.Navigate(new Uri("/View/DataPage.xaml", UriKind.Relative));
+        }
+
         private void Btn_Interlock_Click(object sender, RoutedEventArgs e)
         {
             this.Btn_Dashboard.Style = Application.Current.Resources["menuButton"] as Style;
