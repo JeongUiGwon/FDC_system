5b5ebdbded6cd586d870fe6b1e096e7aae9f8ca9
cms <tyms0503@gmail.com>
2023-04-25 16:02:15 +0900

    Allowed host 변경

--- fdc/app/settings.py
+++ fdc/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = my_setting.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['k8a201.p.ssafy.io','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['k8a201.p.ssafy.io','localhost', '127.0.0.1', '*']
 
 
 # Application definition
