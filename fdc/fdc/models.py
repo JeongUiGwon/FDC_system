c70074e6311e5a5e53a7784d7050a614bf9e0803
cms <tyms0503@gmail.com>
2023-04-25 16:01:45 +0900

    오타 수정 & default 시간데이터 생성 방법 변경

--- fdc/fdc/models.py
+++ fdc/fdc/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime, timedelta
-import random
+import random, string
+from django.apps import apps
+from functools import partial
 
 is_manage = True
 
@@ -10,22 +12,53 @@ def random_past_datetime():
                                       minutes=random.randint(0, 59),
                                       seconds=random.randint(0, 59))
 
-def random_future_datetime(base_datetime):
-    return base_datetime + timedelta(days=random.randint(0, 365),
-                            hours=random.randint(0, 23), 
-                            minutes=random.randint(0, 59), 
-                            seconds=random.randint(0, 59))
+def random_future_datetime_from_past():
+    past_time = random_past_datetime()
+    return past_time + timedelta(days=random.randint(0, 365),
+                                 hours=random.randint(0, 23),
+                                 minutes=random.randint(0, 59),
+                                 seconds=random.randint(0, 59))
 
-time = random_past_datetime()
+
+def generate_id(length):
+    characters = string.ascii_uppercase + string.digits
+    id = ''.join(random.choice(characters) for i in range(length))
+
+    return id
+
+def get_model(table_name):
+    for model in apps.get_models():
+        if model._meta.db_table == table_name:
+            return model
+
+    raise ValueError(f"Model for table '{table_name}' not found.")
+
+def get_pk(model_class):
+    for field in model_class._meta.get_fields():
+        if field.primary_key:
+            return field.name
+
+    raise ValueError(f"pk of '{model_class}' not found.")
+
+def random_id_generate(table_name, length):
+    model_class = get_model(table_name)
+    pk = get_pk(model_class)
+
+    id = generate_id(length)
+    while model_class.objects.filter(**{pk: id}).exists():
+        id = generate_id(length)
+
+    return id
 
 class Equipment(models.Model):
-    equipment_id = models.CharField(max_length=100, primary_key=True)
+    equipment_id = models.CharField(max_length=100, primary_key=True, default=partial)
     equipment_name = models.CharField(max_length=100)
     equipment_state = models.CharField(max_length=10)
-    creater_name = models.CharField(max_length=100)   
-    created_at = models.DateTimeField(default=time)
-    modifier_name = models.CharField(max_length=100)
-    updated_at = models.DateTimeField(default=random_future_datetime(time))
+    creator_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=random_past_datetime)
+    modifier_name = models.CharField(max_length=100, null=True)
+    updated_at = models.DateTimeField(default=random_future_datetime_from_past, null=True)
+
     interlock_id = models.CharField(max_length=100)
 
     class Meta:
@@ -38,10 +71,11 @@ class Param(models.Model):
     param_type = models.CharField(max_length=2)
     param_state = models.IntegerField(default=1)
     out_count = models.IntegerField()
-    creater_name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=time)
-    modifier_name = models.CharField(max_length=100)
-    updated_at = models.DateTimeField(default=random_future_datetime(time))
+    creator_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=random_past_datetime)
+    modifier_name = models.CharField(max_length=100, null=True)
+    updated_at = models.DateTimeField(default=random_future_datetime_from_past, null=True)
+
 
     class Meta:
         db_table = 'param'
@@ -56,10 +90,11 @@ class Recipe(models.Model):
     lsl_action = models.CharField(max_length=100, null=True)
     usl_action = models.CharField(max_length=100, null=True)
     recipe_state = models.IntegerField(default=1)
-    creater_name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=time)
-    modifier_name = models.CharField(max_length=100)
-    updated_at = models.DateTimeField(default=random_future_datetime(time))
+    creator_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=random_past_datetime)
+    modifier_name = models.CharField(max_length=100, null=True)
+    updated_at = models.DateTimeField(default=random_future_datetime_from_past, null=True)
+
 
     class Meta:
         db_table = 'recipe'
@@ -68,7 +103,7 @@ class LotLog(models.Model):
     log_id = models.AutoField(primary_key=True)
     factory_id = models.IntegerField()
     equipment_id = models.IntegerField()
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     recipe_id = models.IntegerField()
     lot_id = models.IntegerField()
 
@@ -80,7 +115,7 @@ class EquipmentState(models.Model):
     id = models.IntegerField(primary_key=True)
     factory_id = models.IntegerField()
     equipment_id = models.IntegerField()
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     mode = models.IntegerField(null=True)
     status = models.IntegerField(null=True)
 
@@ -93,7 +128,7 @@ class ParamLog(models.Model):
     factory_id = models.IntegerField()
     equipment_id = models.IntegerField()
     recipe_id = models.IntegerField()
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     data_list = models.CharField(max_length=200, null=True)
     param_list = models.CharField(max_length=200, null=True)
 
@@ -110,7 +145,7 @@ class InterlockLog(models.Model):
     param_id = models.CharField(max_length=100)
     recipe_id = models.CharField(max_length=100)
     lot_id = models.IntegerField()
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     interlock_type = models.IntegerField()
     out_count = models.IntegerField()
     lower_limit = models.IntegerField(null=True)
@@ -125,7 +160,7 @@ class InterlockLog(models.Model):
 class ParamHistory(models.Model):
     log_id = models.AutoField(primary_key=True)
     action = models.CharField(max_length=20)
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     param_id = models.DateTimeField()
     old_value = models.CharField(max_length=100, null=True)
     new_value = models.CharField(max_length=100, null=True)
@@ -138,7 +173,7 @@ class ParamHistory(models.Model):
 class RecipeHistory(models.Model):
     log_id = models.AutoField(primary_key=True)
     action = models.CharField(max_length=20)
-    created_at = models.DateTimeField(default=time)
+    created_at = models.DateTimeField(default=random_past_datetime)
     recipe_id = models.CharField(max_length=100)
     old_value = models.CharField(max_length=100, null=True)
     new_value = models.CharField(max_length=100, null=True)
