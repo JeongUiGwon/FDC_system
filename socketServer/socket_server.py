b91788da5438b4e4a23485658d4a66e5c8bed3b2
채민기 <ksi03525@naver.com>
2023-04-27 12:32:23 +0900

    🎮[BE:feat] socketServer_v1
    SIM(java) Client - Server - DB data 통신 구현
    
    Releated to S08P31A201-259

--- socketServer/socket_server.py
+++ socketServer/socket_server.py
@@ -1,39 +1,70 @@
-from socket import *;	
-import socket, threading;	
- 	
+import socket, threading, json, pymysql;
+
+storage = []; 
 
 def binder(client_socket, addr):	
-  print('Client Join!! ', addr);	
-  try:	
-    while True:	
-      data = client_socket.recv(1024);	
-      length = int.from_bytes(data, "little"); 
-      data = client_socket.recv(length);	
-      msg = data.decode();	
-      if (length > 0):
-        print('msg : ', msg ,' / Received from : ', addr);	
- 		
-      msg = "상빈 하이 : " + msg;	
-      data = msg.encode();		
-      length = len(data);	
-      client_socket.sendall(length.to_bytes(1024, byteorder='little'));		
-      client_socket.sendall(data);	
-  except:	
-    print("Client Exit~~ " , addr);	
-  finally:		
-    client_socket.close();	
- 	
+    global storage; 
+    print('Join : ', addr);	
+    try:	
+        while True:	
+            data = client_socket.recv(1024);	
+            length = int.from_bytes(data, "little"); 
+            data = client_socket.recv(length);	
+            msg = data.decode();	
+            if (length > 0):
+                print(addr, ' 에서', msg,' 를 보냈습니다.'); 
+                data = json.loads(data); 
+                data["equipment_Id"] = int(data["equipment_Id"]); 
+                storage.append(data); 
+                
+            msg = "서버에서 " + msg + " 의 내용을 받았습니다.";	
+            data = msg.encode();		
+            length = len(data);	
+            client_socket.sendall(length.to_bytes(1024, byteorder='little'));		
+            client_socket.sendall(data); 
+    except:	
+        print("Exit : " , addr);	
+    finally:		
+        client_socket.close(); 
+
+
+
+def saveFile():
+    global storage; 
+    
+    conn = pymysql.connect(host='172.26.6.41',
+                            user='cms',
+                            password='11111111',
+                            db='minki',
+                            charset='utf8'); 
+
+    sql = "INSERT INTO socketTest (equipment_id, equiptment_Name, equipment_State) VALUES (%s, %s, %s)"; 
+
+    with conn:
+            with conn.cursor() as cur:
+                    for i in range(len(storage)):
+                            cur.execute(sql, (storage[i]['equipment_Id'],storage[i]['equiptment_Name'],storage[i]['equipment_State'])); 
+                    conn.commit(); 
+    
+    storage = []; 
+    threading.Timer(60, saveFile).start(); 
+
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);	
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1); 
 server_socket.bind(('172.26.6.41', 8888));	
 server_socket.listen();	
- 	
+storage = []; 	
+
+
 try:		
-  while True:		
-    client_socket, addr = server_socket.accept();	
-    th = threading.Thread(target=binder, args = (client_socket,addr));	
-    th.start();	
+    while True:		
+        client_socket, addr = server_socket.accept();	
+        th1 = threading.Thread(target=binder, args = (client_socket,addr));	
+        th2 = threading.Thread(target=saveFile); 
+        th1.start();	
+        th2.start(); 
 except:	
-  print("server");	
+    print("server");	
 finally:	
-  server_socket.close()
+    server_socket.close(); 
