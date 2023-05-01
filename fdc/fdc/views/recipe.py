46ad694017e2ca4394a1b33d28284d3b0d8b1a28
cms <tyms0503@gmail.com>
2023-05-01 09:30:46 +0900

    미사용 코드 삭제

--- fdc/fdc/views/recipe.py
+++ fdc/fdc/views/recipe.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from fdc.models import Recipe
 from fdc.serializers import RecipeSerializer
-from rest_framework.views import APIView
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
