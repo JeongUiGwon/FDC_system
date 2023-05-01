cb9a1b84d4e04a44414dc697a8fb62cd8ae55d06
cms <tyms0503@gmail.com>
2023-05-01 09:31:57 +0900

    [BE] 인터락 로그 테이블 name 자동 생성

--- fdc/fdc/serializers.py
+++ fdc/fdc/serializers.py
@@ -35,11 +35,22 @@ class InterlockLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = InterlockLog
         fields = '__all__'
+        extra_kwargs = {'equipment_name': {'required': False},
+                        'cause_equip_name': {'required': False}}
+
+        read_only_fields = ('equipment_name', 'cause_equip_name')
+    def create(self, validated_data):
+        equipment = Equipment.objects.get(pk=validated_data['equipment_id'].id)
+        validated_data['equipment_name'] = equipment.equipment_name
+        validated_data['cause_equip_id'] = equipment.equipment_id
+        validated_data['cause_equip_name'] = equipment.equipment_name
+        return super(InterlockLogSerializer, self).create(validated_data)
 
 class ParamHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ParamHistory
         fields = '__all__'
+        read_only_fields = ['param_name']
 
 class RecipeHistorySerializer(serializers.ModelSerializer):
     class Meta:
