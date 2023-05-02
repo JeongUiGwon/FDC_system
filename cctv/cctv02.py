ba5b5c85214e2398a847da3cd645b85c9fc10a7e
isangbin <isangbin96@gmail.com>
2023-05-02 17:38:29 +0900

    🍚[BE:feat] 멀티스레드를 이용한 5분할 동시녹화 및 저장
    Related to:S08P31A201-404

--- cctv/cctv02.py
+++ cctv/cctv02.py
@@ -3,23 +3,24 @@ import pyautogui
 import numpy as np
 import datetime as dt
 import time
+import threading
 
 
-def record():
+def record(equip_name, x, y, w, h):
     now = dt.datetime.now()
     now = now.strftime("%Y_%m_%d_%H_%M_%S")
 
-    print(now + '.avi is recording...')
+    print(equip_name + " - " + now + '.avi is recording...')
 
     # 해상도 설정
-    resolution = (1920,1080)
+    resolution = (w, h)
     # 코덱 설정 (window는 XVID라고함)
     codec = cv2.VideoWriter_fourcc(*'XVID')
     # 저장할 이름 설정 - 단위시간마다 계속 저장할 것이므로 현재시간을 이름으로 가져올 생각
     filename = '{}.avi'.format(now)
     # 저장 경로 설정 - 고정된 경로일 것 같아서 변수화 안해도 될 것이라 생각
-    location = "C:/Users/SSAFY/Desktop/gitlab/자율/CCTV/지옥"
-    fps = 22.0 
+    location = "C:/Users/SSAFY/Desktop/gitlab/자율/CCTV/지옥/"+equip_name
+    fps = 10.0
     # 융합
     out = cv2.VideoWriter(location+'/'+filename, codec, fps, resolution)
 
@@ -28,14 +29,14 @@ def record():
     
     while True:
             # 스크린샷을 계속 찍어 out에 쌓아서 영상을 생성
-            img = pyautogui.screenshot() 
+            img = pyautogui.screenshot(region=(x, y, w, h)) 
             frame = np.array(img) 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
             out.write(frame) 
             
             # 현재 시간이 시작시간으로부터 nn초 이상 흘렀거나 q를 누를 시 스샷누적 종료
-            if time.time() - start_time > 10 or cv2.waitKey(1) == ord('q'):
-                print(now + '.avi is done !')
+            if time.time() - start_time > 60 or cv2.waitKey(1) == ord('q'):
+                print(equip_name + " - " + now + '.avi is done !')
                 break
     
     # 영상 녹화 종료 및 저장
@@ -43,6 +44,20 @@ def record():
     cv2.destroyAllWindows()
 
 
-# 녹화 함수 무한루프
-while True:
-     record()
+def do_record(equip_name, x, y, w, h):
+     while True:
+          record(equip_name, x, y, w, h)
+
+
+thread1 = threading.Thread(target=do_record, args=('설비1', 0, 0, 300, 300))
+thread2 = threading.Thread(target=do_record, args=('설비2', 300, 0, 300, 300))
+thread3 = threading.Thread(target=do_record, args=('설비3', 600, 0, 300, 300))
+thread4 = threading.Thread(target=do_record, args=('설비4', 0, 300, 300, 300))
+thread5 = threading.Thread(target=do_record, args=('설비5', 300, 300, 300, 300))
+
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+thread5.start()
