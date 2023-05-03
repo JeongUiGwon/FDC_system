4ac9d0f51d902108ab835f6b2d43de095ad9bdab
Jeong UiGwon <jek9412@naver.com>
2023-05-03 14:05:14 +0900

    🥑[FE:refactor] 탐색 바 UI 개선

--- win_app/ViewModel/UsersViewModel.cs
+++ win_app/ViewModel/UsersViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +13,12 @@ namespace SOM.ViewModel
 {
     public class UsersViewModel : INotifyPropertyChanged
     {
-        private ObservableCollection<UsersModel> _usersList;
-
         public UsersViewModel()
         {
             SetUsersList();
         }
 
+        private ObservableCollection<UsersModel> _usersList;
         public ObservableCollection<UsersModel> UsersList
         {
             get { return _usersList; }
@@ -29,11 +29,48 @@ namespace SOM.ViewModel
             }
         }
 
+        private ObservableCollection<UsersModel> _filteredUserList;
+        public ObservableCollection<UsersModel> FilteredUserList
+        {
+            get { return _filteredUserList; }
+            set
+            {
+                _filteredUserList = value;
+                OnPropertyChanged(nameof(FilteredUserList));
+            }
+        }
+
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                _searchTerm = value;
+                OnPropertyChanged(nameof(SearchTerm));
+                FilterUserList();
+            }
+        }
+
         private async void SetUsersList()
         {
             FirebaseAdminAuth firebaseAdminAuth = new FirebaseAdminAuth();
             var users = await firebaseAdminAuth.GetUserList();
             UsersList = new ObservableCollection<UsersModel>(users);
+            FilterUserList();
+        }
+
+        private void FilterUserList()
+        {
+            if (UsersList != null && UsersList.Any() && !string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                FilteredUserList = new ObservableCollection<UsersModel>(UsersList.Where(e => e.Email.Contains(SearchTerm) || e.UserName.Contains(SearchTerm)
+                || e.Role.Contains(SearchTerm) || e.Department.Contains(SearchTerm) || e.PhoneNumber.Contains(SearchTerm))); ;
+            }
+            else
+            {
+                FilteredUserList = UsersList;
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
