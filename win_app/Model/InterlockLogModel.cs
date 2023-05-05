76f2183e17b204cfb3789a089ec800c01042caea
Jeong UiGwon <jek9412@naver.com>
2023-05-05 12:00:52 +0900

    🥑[FE:feat] Interlock 기능

---
+++ win_app/Model/InterlockLogModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.Model
+{
+    public class InterlockLogModel
+    {
+        public int log_id { get; set; }
+        public string factory_id { get; set; }
+        public string equipment_name { get; set; }
+        public string cause_equip_id { get; set; }
+        public string cause_equip_name { get; set; }
+        public Nullable<DateTime> created_at { get; set; }
+        public string interlock_type { get; set; }
+        public int out_count { get; set; }
+        public float lower_limit { get; set; }
+        public float upper_limit { get; set; }
+        public float data_value { get; set; }
+        public string cctv_video_url { get; set; }
+        public string equipment { get; set; }
+        public string param { get; set; }
+        public string recipe { get; set; }
+        public string lot { get; set; }
+
+        public InterlockLogModel(int log_id, string factory_id, string equipment_name, string cause_equip_id, string cause_equip_name, string interlock_type, 
+            int out_count, float lower_limit, float upper_limit, float data_value, string cctv_video_url, string equipment, string param, string recipe, 
+            string lot, Nullable<DateTime> Created_at)
+        {
+            this.log_id = log_id;
+            this.factory_id = factory_id;
+            this.equipment_name = equipment_name;
+            this.cause_equip_id = cause_equip_id;
+            this.cause_equip_name = cause_equip_name;
+            this.interlock_type = interlock_type;
+            this.out_count = out_count;
+            this.lower_limit = lower_limit;
+            this.upper_limit = upper_limit;
+            this.data_value = data_value;
+            this.cctv_video_url = cctv_video_url;
+            this.created_at = Created_at;
+            this.equipment = equipment;
+            this.param = param;
+            this.recipe = recipe;
+            this.lot = lot;
+        }
+    }
+}
--- win_app/View/Interlock/InterlockPage.xaml.cs
+++ win_app/View/Interlock/InterlockPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using SOM.Model;
+﻿using Newtonsoft.Json;
+using SOM.Model;
+using SOM.Services;
+using SOM.View.Param;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,15 +30,51 @@ namespace SOM.View.Interlock
         {
             InitializeComponent();
 
-            ObservableCollection<InterlockModel> interlocks = new ObservableCollection<InterlockModel>();
+            // 조회기간 세팅
+            dp_startDate.Text = DateTime.Now.AddMonths(-3).ToString("yyyy-MM-dd");
+            tp_startTime.Text = DateTime.Now.ToString("HH:mm");
+            dp_endDate.Text = DateTime.Today.ToString("yyyy-MM-dd");
+            tp_endTime.Text = DateTime.Now.ToString("HH:mm");
+        }
 
-            interlocks.Add(new InterlockModel("abcd", "권취", "abcd", "권취", "sagwes", "dfkjas", "gddks", 1, 0, 100, 200, "Warning", "Warning", 120, DateTime.Now));
-            interlocks.Add(new InterlockModel("gsdd", "권취", "gsdd", "권취", "gwdsd", "asdge", "bsdxds", 1, 0, 100, 200, "Mailing", "Mailing", 120, DateTime.Now));
-            interlocks.Add(new InterlockModel("gswe", "권취", "gswe", "권취", "gswew", "gdxcd", "wefsd", 1, 0, 100, 200, "Interlock", "Interlock", 120, DateTime.Now));
-            interlocks.Add(new InterlockModel("gdsw", "권취", "gdsw", "권취", "sagwes", "dfkjas", "gddks", 1, 0, 100, 200, "Warning", "Warning", 120, DateTime.Now));
-            interlocks.Add(new InterlockModel("weds", "권취", "weds", "권취", "gsdfd", "wefds", "gdswd", 1, 0, 100, 200, "Warning", "Warning", 120, DateTime.Now));
+        private async void btn_apply_click(object sender, RoutedEventArgs e)
+        {
+            var equipments = dg_equipment.ItemsSource as ObservableCollection<EquipmentsModel>;
+            var selectedEquipments = equipments.Where(el => el.isSelected).ToList();
 
-            InterlockDatagrid.ItemsSource = interlocks;
+            string str_selectedEquipments = string.Join(",", selectedEquipments.Select(el => el.equipment_id));
+
+            string startDate = $"{dp_startDate.Text} {tp_startTime.Text}";
+            string endDate = $"{dp_endDate.Text} {tp_endTime.Text}";
+
+            string str_params = tb_paramID.Text;
+
+            HttpResponseMessage response_getInterlockLog = await GetInterlockLog.GetInterlockLogAsync(str_selectedEquipments, str_params, startDate, endDate);
+            ObservableCollection<InterlockLogModel> content = new ObservableCollection<InterlockLogModel>();
+
+            if (response_getInterlockLog != null && response_getInterlockLog.Content != null)
+            {
+                string str_content = await response_getInterlockLog.Content.ReadAsStringAsync();
+                content = JsonConvert.DeserializeObject<ObservableCollection<InterlockLogModel>>(str_content);
+                dg_interlock.ItemsSource = content;
+            }
+
+        }
+
+        private void dg_equipment_refresh(object sender, RoutedEventArgs e)
+        {
+            dg_equipment.Items.Refresh();
+        }
+
+        private void btn_SearchParams_click(object sender, RoutedEventArgs e)
+        {
+            var modal = new GetParamModal();
+            modal.ShowDialog();
+
+            if (modal.Result != null)
+            {
+                tb_paramID.Text = modal.Result.ToString();
+            }
         }
     }
 }
