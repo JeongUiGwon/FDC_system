ad271aa4d8bf1401e02e64df9837fbddcc154370
최명서 <tyms0503@gmail.com>
2023-05-10 07:20:10 +0000

    [BE] 더미데이터 생성 코드 변경
    Signed-off-by: 최명서 <tyms0503@gmail.com>

--- fdc/fdc/utils/create_dummy_data/param_log_dummy_data.py
+++ fdc/fdc/utils/create_dummy_data/param_log_dummy_data.py
@@ -3,9 +3,10 @@ from ...models import Equipment, Param, Recipe
 from ...utils.create_dummy_data.create_time import random_past_datetime
 
 def generate_dummy_data_param_log():
-    equipment = random.choice(Equipment.objects.all())
-    param = random.choice(Param.objects.all())
     recipe = random.choice(Recipe.objects.all())
+    param = Param.objects.get(pk=recipe.param_id.strip())
+    equipment = Equipment.objects.get(pk=recipe.equipment_id.strip())
+
     value = round(random.uniform(recipe.lsl, recipe.usl), 6)
 
     create_at = random_past_datetime()
