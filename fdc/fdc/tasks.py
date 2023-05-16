67ddfb3b33317af522127daa00b35cdebb3fc244
최명서 <tyms0503@gmail.com>
2023-05-16 09:33:27 +0900

    [BE] autorange
    Signed-off-by: 최명서 <tyms0503@gmail.com>

---
+++ fdc/fdc/tasks.py
@@ -0,0 +1,51 @@
+from celery import shared_task
+from django.db.models import Avg, StdDev
+from .models import AutoRange, Recipe, ParamLog
+
+
+@shared_task
+def apply_auto_range_task(auto_range_id):
+    auto_range = AutoRange.objects.get(id=auto_range_id)
+    param_id = auto_range.param_id
+
+    recipes = Recipe.objects.filter(param__param_id=param_id)
+    print(f'recipes: {recipes}')
+    for recipe in recipes:
+        print(f'recipe: {recipe}')
+        param_logs = ParamLog.objects.filter(recipe_id=recipe)
+        print(f'param_logs: {param_logs}')
+
+        # 데이터가 없다면 함수 종료
+        if not param_logs.exists():
+            return
+        # ParamLog 데이터에서 param_value의 평균과 표준편차를 계산
+        try:
+            mean = param_logs.aggregate(Avg('param_value'))['param_value__avg']
+            std_dev = param_logs.aggregate(StdDev('param_value'))['param_value__stddev']
+            print(f'mean:{mean}, std_dev: {std_dev}')
+            if auto_range.type == 'percent':
+                lower_bound = mean - (mean * auto_range.lsl_weight / 100)
+                upper_bound = mean + (mean * auto_range.usl_weight / 100)
+            elif auto_range.type == 'sigma':
+                lower_bound = mean - auto_range.lsl_weight * std_dev
+                upper_bound = mean + auto_range.usl_weight * std_dev
+            else:
+                return
+        except Exception as e:
+            print(f'error in cal {e}')
+
+        print(recipe.recipe_id)
+        # 해당 recipe 가져옴
+        recipe = Recipe.objects.get(recipe_id=recipe.recipe_id)
+        print(recipe)
+        # USL, LSL 업데이트
+        print(f'lsl: {recipe.lsl} -> {lower_bound}')
+        print(f'usl: {recipe.usl} -> {upper_bound}')
+
+        auto_range.prev_lsl = recipe.lsl
+        auto_range.prev_usl = recipe.usl
+        auto_range.save()
+
+        recipe.lsl = lower_bound
+        recipe.usl = upper_bound
+        recipe.save()
--- fdc/fdc/views/auto_range.py
+++ fdc/fdc/views/auto_range.py
@@ -7,6 +7,9 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from django.db.models import Avg, StdDev
 
+from ..celery import app
+from ..tasks import apply_auto_range_task
+
 
 class AutoRangeViewSet(viewsets.ModelViewSet):
     serializer_class = AutoRangeSerializer
@@ -22,9 +25,11 @@ class AutoRangeViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
 
         if serializer.validated_data.get('is_active'):
-            param_id = serializer.validated_data.get('param').param_id
-            auto_range = AutoRange.objects.get(id=serializer.data['id'])  # Add this line
-            self.apply_auto_range(param_id, auto_range)
+            auto_range = AutoRange.objects.get(id=serializer.data['id'])
+            countdown = auto_range.interval * 60 * 60
+            task = apply_auto_range_task.apply_async((auto_range.id,), countdown=countdown)
+            auto_range.task_id = task.id
+            auto_range.save()
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
@@ -37,55 +42,18 @@ class AutoRangeViewSet(viewsets.ModelViewSet):
         self.perform_update(serializer)
 
         if serializer.validated_data.get('is_active'):
-            param_id = serializer.validated_data.get('param').param_id
-            auto_range = AutoRange.objects.get(id=serializer.data['id'])  # Add this line
-            self.apply_auto_range(param_id, auto_range)
+            auto_range = AutoRange.objects.get(id=serializer.data['id'])
+            # Cancel the previous task if it exists
+            if auto_range.task_id:
+                app.control.revoke(auto_range.task_id, terminate=True)
 
-        return Response(serializer.data)
-
-    @staticmethod
-    def apply_auto_range(param_id, auto_range):  # Add auto_range parameter
-        recipes = Recipe.objects.filter(param__param_id=param_id)
-        print(f'recipes: {recipes}')
-        for recipe in recipes:
-            print(f'recipe: {recipe}')
-            param_logs = ParamLog.objects.filter(recipe_id=recipe)
-            print(f'param_logs: {param_logs}')
-
-            # 데이터가 없다면 함수 종료
-            if not param_logs.exists():
-                return
-            # ParamLog 데이터에서 param_value의 평균과 표준편차를 계산\
-            try:
-                mean = param_logs.aggregate(Avg('param_value'))['param_value__avg']
-                std_dev = param_logs.aggregate(StdDev('param_value'))['param_value__stddev']
-                print(f'mean:{mean}, std_dev: {std_dev}')
-                if auto_range.type == 'percent':
-                    lower_bound = mean - (mean * auto_range.lsl_weight / 100)
-                    upper_bound = mean + (mean * auto_range.usl_weight / 100)
-                elif auto_range.type == 'sigma':
-                    lower_bound = mean - auto_range.lsl_weight * std_dev
-                    upper_bound = mean + auto_range.usl_weight * std_dev
-                else:
-                    return
-            except Exception as e:
-                print(f'error in cal {e}')
-
-            print(recipe.recipe_id)
-            # 해당 recipe 가져옴
-            recipe = Recipe.objects.get(recipe_id=recipe.recipe_id)
-            print(recipe)
-            # USL, LSL 업데이트
-            print(f'lsl: {recipe.lsl} -> {lower_bound}')
-            print(f'usl: {recipe.usl} -> {upper_bound}')
-
-            auto_range.prev_lsl = recipe.lsl
-            auto_range.prev_usl = recipe.usl
+            # Schedule the new task
+            countdown = auto_range.interval * 60 * 60
+            task = apply_auto_range_task.apply_async((auto_range.id,), countdown=countdown)
+            auto_range.task_id = task.id
             auto_range.save()
 
-            recipe.lsl = lower_bound
-            recipe.usl = upper_bound
-            recipe.save()
+        return Response(serializer.data)
 
     def get_queryset(self):
         queryset = AutoRange.objects.all()
